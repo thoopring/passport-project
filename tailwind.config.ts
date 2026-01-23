9e881230a4d3814d89cbe7f28fb94f26d21f7d6f
MinSu <thoopring@gmail.com>
2026-01-23 11:23:57 +0900

    dark globalMap Add

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  // 👇 여기가 핵심! 다크모드를 수동으로 껐다 켰다 할 수 있게 설정
+  // 👇 [핵심] 다크모드를 수동(토글 버튼)으로 껐다 켰다 할 수 있게 설정
   darkMode: "class", 
   
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // 👈 components 폴더도 잘 들어가 있네요!
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
